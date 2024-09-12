7bc3fcffaa1ac285b4c17f0b46d58f256ce9a896
Adam Alive <adamalive.email@gmail.com>
2024-09-12 17:05:29 +0000

    Add handle errors to SignInForm

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -21,6 +21,8 @@ function SignInForm() {
   });
   const { username, password } = signInData;
 
+  const [errors, setErrors] = useState({});
+
   const history = useHistory();
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -28,7 +30,7 @@ function SignInForm() {
       await axios.post("/dj-rest-auth/login/", signInData);
       history.push("/");
     } catch (err) {
-    
+      setErrors(err.response?.data);
     }
   };
   const handleChange = (event) => {
@@ -55,6 +57,11 @@ function SignInForm() {
                 onChange={handleChange}
               />
             </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
 
             <Form.Group controlId="password">
                 <Form.Label className="d-none">Password</Form.Label>
@@ -67,14 +74,23 @@ function SignInForm() {
                     onChange={handleChange}
                 />
               </Form.Group>
+              {errors.password?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
               <Button 
                   className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
                   type="submit"
               >
                 Sign In
               </Button>
+              {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
           </Form>
-
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signup">
