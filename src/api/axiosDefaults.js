1dad12de3e53bfa908ecf37495ec72f706184660
Adam Alive <adamalive.email@gmail.com>
2024-09-13 13:25:39 +0000

    Add axiosReq and axiosRes to axiosDefaults.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,3 +4,6 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://sculpture-drf-api-2aa8ed66624d.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
