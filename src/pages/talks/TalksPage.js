c885b57a72a59cb9564b7639e66a2f1f931baf37
Adam Alive <adamalive.email@gmail.com>
2024-09-22 10:16:13 +0000

    Create TalksPage.js and update App.js

---
+++ src/pages/talks/TalksPage.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import Asset from "../../components/Asset";
+
+import appStyles from "../../App.module.css";
+import styles from "../../styles/TalksPage.module.css";
+
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+
+
+const TalksPage = () => {
+  return (   
+    <Card>
+        Hello
+
+
+    </Card>
+
+  )
+}
+
+export default TalksPage
