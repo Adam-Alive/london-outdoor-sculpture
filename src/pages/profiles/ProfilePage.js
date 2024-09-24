d2a5ad1ac9a24cfbe0a7a939a2ee3b866282fded
Adam Alive <adamalive.email@gmail.com>
2024-09-24 17:35:42 +0000

    Add handleFollow function to ProfilePage

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -30,7 +30,7 @@ function ProfilePage() {
   const [profilePosts, setProfilePosts] = useState({ results: [] });  
   const currentUser = useCurrentUser();
   const { id } = useParams();
-  const setProfileData = useSetProfileData();
+  const { setProfileData, handleFollow } = useSetProfileData();
   const { pageProfile } = useProfileData();
   const [profile] = pageProfile.results;
   const is_owner = currentUser?.username === profile?.owner;
@@ -96,7 +96,7 @@ function ProfilePage() {
             ) : (
               <Button
                 className={`${btnStyles.Button} ${btnStyles.Black}`}
-                onClick={() => {}}
+                onClick={() =>  handleFollow(profile)}
               >
                 follow
               </Button>
