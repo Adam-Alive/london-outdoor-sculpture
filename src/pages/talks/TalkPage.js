338e8aac7663c2a41cff4809646c12a7b9c3d19b
Adam Alive <adamalive.email@gmail.com>
2024-09-25 15:27:58 +0000

    Amend layout on TalkPage

--- src/pages/talks/TalkPage.js
+++ src/pages/talks/TalkPage.js
@@ -31,14 +31,9 @@ function TalkPage() {
 
   return (
     <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        {/* <p>Popular profiles for mobile</p> */}
-        <Talk {...talk.results[0]} setTalks={setTalk} talkPage />
-        {/* <Container className={appStyles.Content}>Comments</Container> */}
-      </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
-      </Col>
+      <Col className="py-2 p-0 p-lg-2" lg={8}>        
+        <Talk {...talk.results[0]} setTalks={setTalk} talkPage />        
+      </Col>    
     </Row>
   );
 }
