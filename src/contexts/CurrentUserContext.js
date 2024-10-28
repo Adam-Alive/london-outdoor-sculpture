3df7d55f4a1d22eced76c3dd553c49d00fec8347
Adam Alive <adamalive.email@gmail.com>
2024-10-28 21:17:45 +0000

    Reset utils.js and CurrentUserContext.js for development

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -2,7 +2,6 @@ import { createContext, useEffect, useState, useMemo, useContext } from "react";
 import axios from "axios";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useHistory } from "react-router-dom";
-import { removeTokenTimestamp, shouldRefreshToken } from "../utils/utils";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
@@ -29,57 +28,52 @@ export const CurrentUserProvider = ({ children }) => {
 // Note empty array as second parameter in line above.
 // This will execute once only until remount.
 
-  useMemo(() => {
-    axiosReq.interceptors.request.use(
-      async (config) => {
-        if (shouldRefreshToken()) {
-          try {
-            await axios.post("/dj-rest-auth/token/refresh/");
-          } catch (err) {
-            setCurrentUser((prevCurrentUser) => {
-              if (prevCurrentUser) {
-                history.push("/signin");
-              }
-              return null;
-            });
-            removeTokenTimestamp();
-            return config;
+useMemo(() => {
+  axiosReq.interceptors.request.use(
+    async (config) => {
+      try {
+        await axios.post("/dj-rest-auth/token/refresh/");
+      } catch (err) {
+        setCurrentUser((prevCurrentUser) => {
+          if (prevCurrentUser) {
+            history.push("/signin");
           }
-        }
+          return null;
+        });
         return config;
-      },
-      (err) => {
-        return Promise.reject(err);
       }
-    );
+      return config;
+    },
+    (err) => {
+      return Promise.reject(err);
+    }
+  );
 
-    axiosRes.interceptors.response.use(
-      (response) => response,
-      async (err) => {
-        if (err.response?.status === 401) {
-          try {
-            await axios.post("/dj-rest-auth/token/refresh/");
-          } catch (err) {
-            setCurrentUser((prevCurrentUser) => {
-              if (prevCurrentUser) {
-                history.push("/signin");
-              }
-              return null;
-            });
-            removeTokenTimestamp();
-          }
-          return axios(err.config);
+  axiosRes.interceptors.response.use(
+    (response) => response,
+    async (err) => {
+      if (err.response?.status === 401) {
+        try {
+          await axios.post("/dj-rest-auth/token/refresh/");
+        } catch (err) {
+          setCurrentUser((prevCurrentUser) => {
+            if (prevCurrentUser) {
+              history.push("/signin");
+            }
+            return null;
+          });
         }
-        return Promise.reject(err);
+        return axios(err.config);
       }
-    );
-  }, [history]);
-
+      return Promise.reject(err);
+    }
+  );
+}, [history]);
   return (
     <CurrentUserContext.Provider value={currentUser}>
-      <SetCurrentUserContext.Provider value={setCurrentUser}>
-        {children}
-      </SetCurrentUserContext.Provider>
+        <SetCurrentUserContext.Provider value={setCurrentUser}>
+            {children}
+        </SetCurrentUserContext.Provider>
     </CurrentUserContext.Provider>
   );
-  };
+};
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,4 @@
 import { axiosReq } from "../api/axiosDefaults";
-import jwtDecode from 'jwt-decode';
 
 export const fetchMoreData = async (resource, setResource) => {
   try {
@@ -48,17 +47,4 @@ export const unfollowHelper = (profile, clickedProfile) => {
       { ...profile, following_count: profile.following_count - 1 }
     : 
       profile;
-};
-
-export const setTokenTimestamp = (data) => {
-  const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
-  localStorage.setItem('refreshTokenTimestamp', refreshTokenTimestamp);
-};
-
-export const shouldRefreshToken = () => {
-  return !!localStorage.getItem('refreshTokenTimestamp');
-};
-
-export const removeTokenTimestamp = () => {
-  localStorage.removeItem('refreshTokenTimestamp');
 };
