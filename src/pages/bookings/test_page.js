558c1cd3ecc1f00246ea4676c962d5acb80866bf
Adam Alive <adamalive.email@gmail.com>
2024-10-28 18:22:37 +0000

    Update test_page.js

--- src/pages/bookings/test_page.js
+++ src/pages/bookings/test_page.js
@@ -1,103 +1,72 @@
-// Terst code:
-useEffect(() => {
-  const handleMount = async () => {
-    if (currentUser?.profile_id?.toString() === id) {
-      try {
-        const { data } = await axiosReq.get(`/profiles/${id}/`);
-        const { name, content, image } = data;
-        setProfileData({ name, content, image });
-      } catch (err) {
-        console.log(err);
-        history.push("/");
-      }
-    } else {
-      history.push("/");
-    }
-  };
 
+// // Test for BookingPage.js
 
+// import React, { useEffect, useState } from "react";
 
+// import Col from "react-bootstrap/Col";
+// import Row from "react-bootstrap/Row";
+// import Container from "react-bootstrap/Container";
 
+// import appStyles from "../../App.module.css";
+// import { useParams } from "react-router-dom";
+// import { axiosReq } from "../../api/axiosDefaults";
+// import Booking from "./Booking";
+// import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
+// // import InfiniteScroll from "react-infinite-scroll-component";
+// // import Asset from "../../components/Asset";
+// // import { fetchMoreData } from "../../utils/utils";
 
-
-// Test for BookingPage.js
-
-import React, { useEffect, useState } from "react";
-
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
-
-import appStyles from "../../App.module.css";
-import { useParams } from "react-router-dom";
-import { axiosReq } from "../../api/axiosDefaults";
-import Booking from "./Booking";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
-
-// import InfiniteScroll from "react-infinite-scroll-component";
-// import Asset from "../../components/Asset";
-// import { fetchMoreData } from "../../utils/utils";
-
-function BookingPage() {
-    const { id } = useParams();
-    const [booking, setBooking] = useState({ results: [] });
+// function BookingPage() {
+//     const { id } = useParams();
+//     const [booking, setBooking] = useState({ results: [] });
   
-    const currentUser = useCurrentUser();
-    const profile_id = currentUser?.profile_id;
-    // See ProfileEditForm Line 39 to validate booking / profile_id
+//     const currentUser = useCurrentUser();
+//     const profile_id = currentUser?.profile_id;
+//     // See ProfileEditForm Line 39 to validate booking / profile_id
 
-    useEffect(() => {
-        const handleMount = async () => {
-          try {
-            const [{ data: booking }] = await Promise.all([
-              axiosReq.get(`/bookings/${id}`),              
-            ]);
-            setBooking({ results: [booking] });          
-          } catch (err) {
-            console.log(err);
-          }
-        };
+//     useEffect(() => {
+//         const handleMount = async () => {
+//           try {
+//             const [{ data: booking }] = await Promise.all([
+//               axiosReq.get(`/bookings/${id}`),              
+//             ]);
+//             setBooking({ results: [booking] });          
+//           } catch (err) {
+//             console.log(err);
+//           }
+//         };
     
-        // Take this from BookingEditForm line 38 and try!!
-         const { data } = await axiosReq.get(`/bookings/${id}/`);
+//         // Take this from BookingEditForm line 38 and try!!
+//          const { data } = await axiosReq.get(`/bookings/${id}/`);
 
 
-        handleMount();
-      }, [id]);
+//         handleMount();
+//       }, [id]);
 
-    return (
-    <Container className={styles.Content}>
-      <h3>My Bookings</h3>
-      <br />      
-        {hasLoaded ? (
-          <>
-            {booking.results.length ? (
-              booking.results.map((booking) => (
-                <Booking key={booking.id} {...booking} setBookings={setBooking} bookings={bookings} />
-              ))
-            ) : (
-              <Container className={appStyles.Content}>
-                <Asset src={NoResults} />
-              </Container>
-            )}
-          </>
-        ) : (
-          <Container className={appStyles.Content}>
-            <Asset spinner />
-          </Container>
-        )}  
-    </Container>
-  );
-};
+//     return (
+//     <Container className={styles.Content}>
+//       <h3>My Bookings</h3>
+//       <br />      
+//         {hasLoaded ? (
+//           <>
+//             {booking.results.length ? (
+//               booking.results.map((booking) => (
+//                 <Booking key={booking.id} {...booking} setBookings={setBooking} bookings={bookings} />
+//               ))
+//             ) : (
+//               <Container className={appStyles.Content}>
+//                 <Asset src={NoResults} />
+//               </Container>
+//             )}
+//           </>
+//         ) : (
+//           <Container className={appStyles.Content}>
+//             <Asset spinner />
+//           </Container>
+//         )}  
+//     </Container>
+//   );
+// };
 
-export default BookingPage;
-
-
-
-{is_owner && postPage && (
-  <MoreDropdown
-    handleEdit={handleEdit}
-    handleDelete={handleDelete}
-  />
-)}
+// export default BookingPage;
