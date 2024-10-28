0e35c19c8602ca6803f23bd3ef6beab8932e5b4e
Adam Alive <adamalive.email@gmail.com>
2024-10-28 16:12:49 +0000

    Update fetchBookings() in Bookings.js

--- src/pages/bookings/Bookings.js
+++ src/pages/bookings/Bookings.js
@@ -13,11 +13,9 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Booking from './Booking';
 import NoResults from "../../assets/no-results.png";
 
-
-
 function Bookings() {
   const currentUser = useCurrentUser();
-  // const is_owner = currentUser?.username === owner;
+  const is_owner = currentUser?.username === owner;
   const { id } = useParams();
   const history = useHistory();
   
@@ -28,7 +26,7 @@ function Bookings() {
     const fetchBookings = async () => {
       if (currentUser?.profile_id?.toString() === id) {
         try {
-          const { data } = await axiosReq.get(`/bookings/`);
+          const { data } = await axiosReq.get(`/bookings/?owner__profile=${id}`);
           setBookings(data);
           setHasLoaded(true);
           console.log(data);
