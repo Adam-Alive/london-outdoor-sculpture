39f2c7ed462cbfb9c49f769af81226ba51a1a0e0
Adam Alive <adamalive.email@gmail.com>
2024-10-29 17:58:54 +0000

    Add styling to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,13 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import styles from "../styles/Footer.module.css"
 
 const Footer = () => {
   return (
     <footer>
         <Container fluid>
-            <Row className="bg-primary text-white">
+            <Row className={styles.Row}>
                 <NavLink to="/">                
                 </NavLink>                
                 Footer Section
