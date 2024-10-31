1ade52d2301a06cd1c912d443c2d4905ff316576
Adam Alive <adamalive.email@gmail.com>
2024-10-31 14:30:41 +0000

    Tidy up code in Bookings.js

--- src/pages/bookings/Bookings.js
+++ src/pages/bookings/Bookings.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { useHistory, useParams } from "react-router-dom";
-
+import { useParams } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 
 import Asset from "../../components/Asset";
-
 import appStyles from "../../App.module.css";
 import styles from "../../styles/Bookings.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
@@ -20,7 +18,6 @@ import { fetchMoreData } from "../../utils/utils";
 function Bookings() {
   const currentUser = useCurrentUser();
   const { id } = useParams();
-  const history = useHistory();
    
   const [bookings, setBookings] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
@@ -31,8 +28,7 @@ function Bookings() {
         try {
           const { data } = await axiosReq.get(`/bookings/?owner__profile=${currentUser?.profile_id}`);
           setBookings(data);
-          setHasLoaded(true);
-          // console.log(data);
+          setHasLoaded(true);    
         } catch (err) {
           console.log(err);
         } 
@@ -44,7 +40,7 @@ function Bookings() {
     return () => {
       clearTimeout(timer);
     };    
-  }, [currentUser?.profile_id, history, id, pathname]);
+  }, [currentUser?.profile_id, id, pathname]);
 
   return (
     <Container className={styles.Content}>
